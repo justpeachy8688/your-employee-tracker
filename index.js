17ab580e35ea4880e0c3bd69e95c13ac0a7250aa
Taylor Sanders <tsanders3515@gmail.com>
2021-03-18 12:10:01 -0800

    push for LA

--- index.js
+++ index.js
@@ -6,48 +6,48 @@ let queries = require("./data/index.js");
 
 
 async function basicQuestions() {
-    const {choice} = await inquirer.prompt([
+    const { choice } = await inquirer.prompt([
         {
-        name: "choice",
-        type: "rawlist",
-        message: "What would you like to do?",
-        choices: [
-             "View all employees",
-        "View all departments",
-        "View all roles",
-        "Add employee",
-        "Add role",
-        "Add department",
-        "Update employee role",
-        ]
-    }]);
-        //Switch case depending on each user option
-        switch (choice) {
+            name: "choice",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all roles",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Update employee role",
+            ]
+        }]);
+    //Switch case depending on each user option
+    switch (choice) {
         case 'View all employees':
-          return viewAllEmp();
-     
+            return viewAllEmp();
+
         case 'View all departments':
-          return viewAllDepts();
+            return viewAllDepts();
 
         case 'View all roles':
-          return viewAllRoles();
+            return viewAllRoles();
 
         case 'Add employee':
-          return addEmployee();
-          
+            return addEmployee();
+
         case 'Add role':
-          return addRole();
+            return addRole();
 
         case 'Add department':
-          return addDept();
+            return addDept();
 
         case 'Update employee role':
-          return updateEmpRole();
+            return updateEmpRole();
     }
 }
 // ADD DEPARTMENT
 async function addDept() {
-    const department = await inquirer.prompt ([
+    const department = await inquirer.prompt([
         {
             name: "name",
             message: "What is the name of the department?"
@@ -60,10 +60,10 @@ async function addDept() {
 
 // ADD ROLE
 async function addRole() {
-    const role = await inquirer.prompt ([
+    const role = await inquirer.prompt([
         {
-        name: "title",
-        message: 'What role would you like to add?'
+            name: "title",
+            message: 'What role would you like to add?'
         },
         {
             name: "salary",
@@ -82,7 +82,7 @@ async function addRole() {
 
 // //ADD EMPLOYEE
 async function addEmployee() {
-    const emp = await inquirer.prompt ([
+    const emp = await inquirer.prompt([
         {
             name: "first_name",
             message: 'What is their first name?'
@@ -108,16 +108,16 @@ async function addEmployee() {
 //VIEW ALL EMPLOYEES
 async function viewAllEmp() {
     // SELECT * FROM employee;
-    let query = "SELECT * FROM employee";
+    const query = "SELECT * FROM employee";
     const rows = await database.query(query);
     console.table(rows);
 
-basicQuestions();
+    basicQuestions();
 }
 
 //VIEW ALL DEPARTMENTS
 async function viewAllDepts() {
-    let query = "SELECT * FROM department";
+    const query = "SELECT * FROM department";
     const rows = await database.query(query);
     console.table(rows);
 
@@ -126,15 +126,54 @@ async function viewAllDepts() {
 
 //VIEW ALL ROLES
 async function viewAllRoles() {
-let query = 'SELECT * FROM role';
-const rows = await database.query(query);
-console.table(rows);
+    const query = 'SELECT * FROM role';
+    const rows = await database.query(query);
+    console.table(rows);
 
-basicQuestions();
+    basicQuestions();
 }
 
 //UPDATE EMPLOYEE ROLE
+async function updateEmpRole() {
+    const employees = await database.query('SELECT id, CONCAT(first_name, " ", last_name AS name FROM employee');
 
+    const employeeChoices = employees.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+    }));
+
+    inquirer.prompt([
+        {
+            name: "empID",
+            type: "list",
+            message: "For which employee?",
+            choices: employeeChoices
+        }
+    ])
+    // .then(empAnswers => {
+
+    // })
+
+    const roles = await database.query('SELECT id, CONCAT(title , salary, department_id AS title FROM role');
+
+    const roleChoices = roles.map(role => ({
+        name: `${role.title} ${role.salary}`,
+        value: role.id
+    }));
+
+    inquirer.prompt([
+        {
+            name: "newRole",
+            type: "list",
+            message: "Change their role to:",
+            choices: roleChoices
+        }
+    ])
+
+    // .then(answers => {
+
+    //     })
+}
 
 
 basicQuestions();
